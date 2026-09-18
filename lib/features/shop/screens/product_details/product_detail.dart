2769c06c7141721b0186103d63d63efe523a8265
Maissam110 <alimaissam18@gmail.com>
2026-09-18 21:38:15 +0500

    produt details

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax_flutter/iconsax_flutter.dart';
-import 'package:shopping_app/common/widgets/appbar/appbar.dart';
-import 'package:shopping_app/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
-import 'package:shopping_app/common/widgets/icon/circular_icon.dart';
-import 'package:shopping_app/common/widgets/images/rounded_image.dart';
 import 'package:shopping_app/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
 import 'package:shopping_app/features/shop/screens/product_details/widgets/product_meta_data.dart';
 import 'package:shopping_app/features/shop/screens/product_details/widgets/rating_share_widget.dart';
-import 'package:shopping_app/util/constants/colors.dart';
-import 'package:shopping_app/util/constants/image_strings.dart';
+
 import 'package:shopping_app/util/constants/sizes.dart';
 import 'package:shopping_app/util/helpers/helper_function.dart';
 
